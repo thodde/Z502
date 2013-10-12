be314df43341783883b6a016e37c00d9f11b306f
azafft <azafft@enernoc.com>
2013-10-12 12:06:21 -0400

    squaring out linked list add functionality

--- list.c
+++ list.c
@@ -8,20 +8,56 @@ LinkedList create_list() {
         return NULL;
     }
 
+    list->data = NULL;
     list->next = NULL;
     return list;
 }
 
 int add_to_list( LinkedList l, PCB* p) {
-    LinkedList new_node = (LinkedList) calloc(1, sizeof(Node));
-    
-    if (new_node == NULL) {
-        printf("Could not create node...");
-        return 0;
+    if (l->data == NULL)  {
+        // The list is empty, so add to the front
+        l->data = p;
+        l->next = NULL;
+        return 1;
     }
 
+    if (l->data->delay > p->delay) {
+        // The list is not empty, but the new entry is triggered earlier than the current head ptr
+        Node *new_node = (Node*) calloc (1, sizeof(Node));
+        new_node->next = l->next;
+        l->next = new_node;
+        new_node->data = l->data;
+        l->data = p;
+
+        return 1;
+    }
+
+    // Otherwise iterate through the list until we find the appropriate spot to nestle the new node
+    Node *cursor = l;
+    Node *prev = NULL;
+
+    while (cursor != NULL && (cursor->data->delay < p->delay)) {
+        prev = cursor;
+        cursor = cursor->next;
+    }
+
+    //this is not an ordered list right now
+    Node *new_node = (Node*) calloc (1, sizeof(Node));
+    new_node->next = prev->next;
+    prev->next = new_node;
     new_node->data = p;
-    new_node->next = l->next;
-    l->next = new_node;
+
+
+
+//    LinkedList new_node = (LinkedList) calloc(1, sizeof(Node));
+//
+//    if (new_node == NULL) {
+//        printf("Could not create node...");
+//        return 0;
+//    }
+//
+//    new_node->data = p;
+//    new_node->next = l->next;
+//    l->next = new_node;
     return 1;
 }
